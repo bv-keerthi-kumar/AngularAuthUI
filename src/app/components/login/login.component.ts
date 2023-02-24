1d563e73a66e060936a14024318053083f53ac88
bvkeerthi <bvkeerthi123@gmail.com>
2023-02-24 09:42:06 +0530

    Changes

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import{FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validateform';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+loginForm !: FormGroup;
+
+constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username:['', Validators.required],
+      password:['',Validators.required]
+    })
+  }
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value);
+      console.log(1 + "2" + "2");
+      console.log(1 + +"2" + "2");
+      console.log(1 + -"1" + "2");
+      console.log(+"1" + "1" + "2");
+      //console.log("A" - "B" + "2");
+      //console.log("A" - "B" +2);
+
+      // send obj to database
+    }else{
+
+      //throw an error using toaster and with required fields
+      console.log("Form is not valid");
+      ValidateForm.validateAllFormFields(this.loginForm);
+    }
+
+  }
+
 
 }
+
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validateform';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+
+  signUpForm !: FormGroup
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      firstname :['', Validators.required],
+      lastname:['', Validators.required],
+      email:['',Validators.required],
+      username:['',Validators.required],
+      password: ['',Validators.required]
+    })
+  }
+
+  onSignUp(){
+    if(this.signUpForm.valid){
+      console.log(this.signUpForm.value);
+      // send obj to database
+    }else{
+
+      //throw an error using toaster and with required fields
+      console.log("Form is not valid");
+      ValidateForm.validateAllFormFields(this.signUpForm);
+    }
+
+  }
+
 
 }
